8669597447b24aafc68b99fc69244595e835aff3
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-10 13:12:11 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-game:GetService("ReplicatedStorage").ScreenOwners.Owner:FireServer()
+game:GetService("ReplicatedStorage").RNGStreakCenes.Chromatic_Genesis:FireServer()
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Script By Nexer" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
